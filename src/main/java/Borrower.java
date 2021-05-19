0c76a6ed39acb6b3c38cde5bdd070ad6a57e5733
konrad <ktalaga90@gmail.com>
2021-05-19 07:09:15 +0100

    Borrower and BorrowerTest classes added

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Borrower {
+
+    private ArrayList<Book> booksBorrowed;
+
+    public Borrower(){
+        this.booksBorrowed = new ArrayList<Book>();
+    }
+
+}
---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+public class BorrowerTest {
+
+    Borrower borrower;
+
+    @Before
+    public void before(){
+        borrower = new Borrower();
+    }
+
+}
